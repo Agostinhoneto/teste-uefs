9cfd60542f4d7dddeba0ab019424507c817f03d9
Agostinhoneto <agostneto6@gmail.com>
2024-01-11 13:26:19 -0300

    criando teste de tag

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tag;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,8 +14,9 @@ class TagTest extends TestCase
      */
     public function test_example(): void
     {
-        $response = $this->get('/');
+        $tag = Tag::factory()->create();
 
-        $response->assertStatus(200);
+        $this->assertInstanceOf(Tag::class, $tag);
+        
     }
 }
